bcdf5765cace190d4823a4390771c11140609f32
piscopato <126119781+piscopato@users.noreply.github.com>
2024-01-05 20:20:31 -0300

    Update clima.py

--- clima.py
+++ clima.py
@@ -3,7 +3,7 @@ import requests
 
 def clima_estado(city):
     try:
-        API_KEY = "9f441d521e13bc0ee295186fedc05e19" # informe sua key de api do openweather
+        API_KEY = "" # informe sua key de api do openweather
         cidade = city
         link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&lang=pt_br"
         print(link)
