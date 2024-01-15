7f51d1aa80e073cc639f63f41f08b8d08b484695
piscopato <126119781+piscopato@users.noreply.github.com>
2024-01-15 18:45:13 -0300

    Delete isto.py

--- isto.py
+++
@@ -1,9 +0,0 @@
-import os
-
-arquivos = os.listdir('D:\\novochatbot\\videos')
-
-if 'JOGOS pelo MEMORY CARD DO PS2! Conheça oMX4SIO!.mp3' in arquivos:
-    print('está aqui')
-else:
-    print('não está')
-
